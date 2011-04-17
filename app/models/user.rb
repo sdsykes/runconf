5b6f9f185ca00dc39e0885a27701729f338efad9
Alexander Lang <alex@upstre.am>
2011-04-17 12:17:39 +0200

    the user info from github looks different than i expected

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include CouchPotato::Persistence
   
   property :name
+  property :email
   
   view :by_id, key: :_id
   view :by_name, key: :name
--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,5 +1,5 @@
 Given /^I am signed in as "([^"]+)"$/ do |name|
-  page.driver.post(session_path, {}, {'omniauth.auth' => {'uid' => 1, 'name' => name}})
+  page.driver.post(session_path, {}, {'omniauth.auth' => {'uid' => 1, 'user_info' => {'name' => name, 'email' => "#{name}@example.com"}}})
 end
 
 When /^I sign out$/ do
